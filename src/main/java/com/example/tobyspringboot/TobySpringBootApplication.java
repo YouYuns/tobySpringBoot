ea5be9fe23cd57f41df30cb2e7021ef39b6885b8
Youyuns <tjdgh0312@naver.com>
2023-02-15 11:32:41 +0900

    addServlet만들고 매핑하기

--- src/main/java/com/example/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringBootApplication.java
@@ -1,13 +1,32 @@
 package com.example.tobyspringboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobySpringBootApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer((ServletContextInitializer)
+                servletContext -> {
+                    servletContext.addServlet("hello", new HttpServlet() {
+                        @Override
+                        public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+                            res.setStatus(200); //응답코드
+                            res.setHeader("Content-Type", "text/plain"); //헤더
+                            res.getWriter().println("Hello Servlet");//바디
+                        }
+                    }).addMapping("/hello");
+                });
+        webServer.start();
     }
-
 }
