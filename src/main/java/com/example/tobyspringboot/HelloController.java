a3d57d17de9aa3a6234b70c636f0537925b214b4
YouYuns <98165575+YouYuns@users.noreply.github.com>
2023-02-19 16:11:11 +0900

    컨트롤러 매핑과 바인딩

--- src/main/java/com/example/tobyspringboot/HelloController.java
+++ src/main/java/com/example/tobyspringboot/HelloController.java
@@ -4,9 +4,7 @@ package com.example.tobyspringboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class HelloController {
-    @GetMapping("/hello")
     public String hello(String name){
         return "Hello " + name;
     }
