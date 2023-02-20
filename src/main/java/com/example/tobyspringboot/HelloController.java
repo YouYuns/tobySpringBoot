2e1088f6f7606cf7866ae9464a1a36df0d79868e
Youyuns <tjdgh0312@naver.com>
2023-02-20 19:47:36 +0900

    자바코드 구성정보사용

--- src/main/java/com/example/tobyspringboot/HelloController.java
+++ src/main/java/com/example/tobyspringboot/HelloController.java
@@ -2,11 +2,21 @@ package com.example.tobyspringboot;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+
 
 public class HelloController {
+
+
+    private final HelloService helloService;
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
     public String hello(String name){
-        SimpleHelloService helloService = new SimpleHelloService();
-        return helloService.sayHello(name);
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
