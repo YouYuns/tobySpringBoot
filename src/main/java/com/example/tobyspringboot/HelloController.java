8e04ae1190a0057c0dfa6df6ba1ed7eece0ffb9e
YouYuns <98165575+YouYuns@users.noreply.github.com>
2023-02-21 22:50:21 +0900

    Component스캔

--- src/main/java/com/example/tobyspringboot/HelloController.java
+++ src/main/java/com/example/tobyspringboot/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.tobyspringboot;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 
 
-public class HelloController {
 
+@RestController
+public class HelloController {
 
     private final HelloService helloService;
     public HelloController(HelloService helloService) {
--- src/main/java/com/example/tobyspringboot/MyComponent.java
+++ src/main/java/com/example/tobyspringboot/MyComponent.java
@@ -1,7 +1,14 @@
 package com.example.tobyspringboot;
 
-import java.lang.annotation.Retention;
+import org.springframework.stereotype.Component;
 
-@Retention()
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Component
 public @interface MyComponent {
 }
