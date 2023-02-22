5cf9d8e9ababbdc1df874d764b8f93bf8a89ed71
Youyuns <tjdgh0312@naver.com>
2023-02-22 19:29:44 +0900

    메타애노테이션

--- src/test/java/com/example/tobyspringboot/HelloServiceTest.java
+++ src/test/java/com/example/tobyspringboot/HelloServiceTest.java
@@ -3,9 +3,20 @@ package com.example.tobyspringboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface UnitTest{
+
+}
 public class HelloServiceTest {
 
-    @Test
+    @UnitTest
     void simpleHelloService(){
         SimpleHelloService helloService = new SimpleHelloService();
 
